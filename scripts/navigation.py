be1b489efa18d29053633c75898e6390d292a847
Nenetti <akinori1090@gmail.com>
2019-02-08 06:03:35 +0900

    first commit

---
+++ scripts/navigation.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+import actionlib
+from actionlib_msgs.msg import GoalID
+from actionlib_msgs.msg import GoalStatus
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import Header, String
+from geometry_msgs.msg import Point, Quaternion
+from location.srv import *
+
+
+class Navigation:
+    pub = None  # type: rospy.Publisher
+
+    def start(self):
+
+        def navigation_callback(message):
+            # type: (String) -> None
+            print(message.data)
+            rospy.wait_for_service("location/request")
+            try:
+                request = rospy.ServiceProxy('location/request', Request_Location)
+                response = request(message.data)  # type: Request_LocationResponse
+                client = actionlib.SimpleActionClient("/move_base", MoveBaseAction)
+                client.wait_for_server()
+                goal = MoveBaseGoal()
+                goal.target_pose.header.stamp = rospy.Time.now()
+                goal.target_pose.header.frame_id = "map"
+                goal.target_pose.pose = response.location.pose
+                client.send_goal(goal)
+                client.wait_for_result()
+                if client.get_state() == GoalStatus.SUCCEEDED:
+                    print("SUCCEEDED")
+                if client.get_state() == GoalStatus.ABORTED:
+                    print("ABORTED")
+            except rospy.ServiceException as e:
+                rospy.ERROR(e)
+                return
+            '''
+            
+
+            return client.get_result()  # A FibonacciResult 
+            '''
+
+        rospy.init_node('navigation', anonymous=False)
+        rospy.Subscriber("/navigation/start", String, navigation_callback)
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    Navigation().start()
